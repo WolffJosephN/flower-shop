982b2dbd47704ce52cd8381e8fbdb0d8865d2c41
WolffJosephN <wolff.joseph.n@gmail.com>
2020-12-05 11:03:08 -0600

    Added the userSchema and got the basic info filled in for now. This will change later when I figure out how I want to do my user validation for log in purposes.

---
+++ fs-back/models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 5,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    addresses: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
